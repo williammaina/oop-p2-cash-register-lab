15380156148e07d2e44095ddd16b95fb032fce6a
williammaina <wmaina8442@gmail.com>
2026-06-07 00:18:09 +0300

    refactored code to Implement CashRegister class with discount and transaction management

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -6,27 +6,16 @@ class CashRegister:
         self.previous_transactions = []
 
         if isinstance(discount, int) and 0 <= discount <= 100:
-            self._discount = discount
+            self.discount = discount
         else:
             print("Not valid discount")
-            self._discount = 0
+            self.discount = 0
 
-    @property
-    def discount(self):
-        return self._discount
-
-    @discount.setter
-    def discount(self, value):
-        if isinstance(value, int) and 0 <= value <= 100:
-            self._discount = value
-        else:
-            print("Not valid discount")
-
-    def add_item(self, item, price, quantity):
+    def add_item(self, item, price, quantity=1):
         self.total += price * quantity
 
-        for _ in range(quantity):
-            self.items.append(item)
+        # Only add item once to items list
+        self.items.append(item)
 
         self.previous_transactions.append({
             "item": item,
@@ -35,25 +24,19 @@ class CashRegister:
         })
 
     def apply_discount(self):
-        if len(self.previous_transactions) == 0:
+        if self.discount == 0:
             print("There is no discount to apply.")
             return
 
-        discount_amount = self.total * (self.discount / 100)
-        self.total -= discount_amount
+        self.total -= self.total * (self.discount / 100)
 
     def void_last_transaction(self):
-        if len(self.previous_transactions) == 0:
+        if not self.previous_transactions:
             return
 
-        last_transaction = self.previous_transactions.pop()
+        transaction = self.previous_transactions.pop()
 
-        item = last_transaction["item"]
-        price = last_transaction["price"]
-        quantity = last_transaction["quantity"]
+        self.total -= transaction["price"] * transaction["quantity"]
 
-        self.total -= price * quantity
-
-        for _ in range(quantity):
-            if item in self.items:
-                self.items.remove(item)
+        if transaction["item"] in self.items:
+            self.items.remove(transaction["item"])
